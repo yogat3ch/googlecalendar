4f94ba9410fe866a442b2d0009ba49258b43eccb
Stephen Holsenbeck <stephen@virgalabs.io>
2023-06-05 10:18:55 -0400

    Fix event_ls print method stack overflow

--- R/class-event_ls.R
+++ R/class-event_ls.R
@@ -2,7 +2,7 @@ as.event_ls <- function(x) UseMethod("as.event_ls", x)
 
 as.event_ls.data.frame <- function(x) {
 
-  structure(dplyr::tbl_df(data.frame(
+  out <- tibble::tibble(
     etag = x$etag,
     id = x$id,
     status = x$status %||% NA_character_,
@@ -16,18 +16,18 @@ as.event_ls.data.frame <- function(x) {
     creator.id = x$creator.id %||% NA_character_,
     creator.email = x$creator.email %||% NA_character_,
     creator.displayName = x$creator.displayName %||% NA_character_,
-    creator.self = x$creator.self %||% FALSE %NA% FALSE,
+    creator.self = (x$creator.self %||% FALSE) %|% FALSE,
     organizer.id = x$organizer.id %||% NA_character_,
     organizer.email = x$organizer.email %||% NA_character_,
     organizer.displayName = x$organizer.displayName %||% NA_character_,
-    organizer.self = x$organizer.self %||% FALSE %NA% FALSE,
+    organizer.self = (x$organizer.self %||% FALSE) %|% FALSE,
     start.date = x$start.date %||% NA_character_,
     start.dateTime = x$start.dateTime,
     start.timeZone = x$start.timeZone %||% NA_character_,
     end.date = x$end.date %||% NA_character_,
     end.dateTime = x$end.dateTime,
     end.timeZone = x$end.timeZone %||% NA_character_,
-    endTimeUnspecified = x$endTimeUnspecified %||% FALSE %NA% FALSE,
+    endTimeUnspecified = (x$endTimeUnspecified %||% FALSE) %|% FALSE,
     recurringEventId = x$recurringEventId %||% NA_character_,
     originalStartTime.date =
       x$originalStartTime.date %||% NA_character_,
@@ -35,31 +35,24 @@ as.event_ls.data.frame <- function(x) {
       x$originalStartTime.dateTime %||% NA_character_,
     originalStartTime.timeZone =
       x$originalStartTime.timeZone %||% NA_character_,
-    transparency = x$transparency %||% "opaque" %NA% "opaque",
-    visibility = x$visibility %||% "default" %NA% "default",
+    transparency = (x$transparency %||% "opaque") %|% "opaque",
+    visibility = (x$visibility %||% "default") %|% "default",
     iCalUID = x$iCalUID,
     sequence = x$sequence,
-    attendeesOmitted = x$attendeesOmitted %||% FALSE %NA% FALSE,
+    attendeesOmitted = (x$attendeesOmitted %||% FALSE) %|% FALSE,
     hangoutLink = x$hangoutLink %||% NA_character_,
-    anyoneCanAddSelf = x$anyoneCanAddSelf %||% FALSE %NA% FALSE,
-    guestsCanInviteOthers = x$guestsCanInviteOthers %||% TRUE %NA% TRUE,
-    guestsCanModify = x$guestsCanModify %||% FALSE %NA% FALSE,
+    anyoneCanAddSelf = (x$anyoneCanAddSelf %||% FALSE) %|% FALSE,
+    guestsCanInviteOthers = (x$guestsCanInviteOthers %||% TRUE) %|% TRUE,
+    guestsCanModify = (x$guestsCanModify %||% FALSE) %|% FALSE,
     guestsCanSeeOtherGuests =
-      x$guestsCanSeeOtherGuests %||% TRUE %NA% TRUE,
-    privateCopy = x$privateCopy %||% FALSE %NA% FALSE,
-    locked = x$locked %||% FALSE %NA% FALSE,
-    reminders.useDefault = x$reminders.useDefault %||% TRUE %NA% TRUE,
+      (x$guestsCanSeeOtherGuests %||% TRUE) %|% TRUE,
+    privateCopy = (x$privateCopy %||% FALSE) %|% FALSE,
+    locked =( x$locked %||% FALSE) %|% FALSE,
+    reminders.useDefault = (x$reminders.useDefault %||% TRUE) %|% TRUE,
     source.url = x$source.url %||% NA_character_,
-    source.title = x$source.title %||% NA_character_,
-    stringsAsFactors = FALSE
-  )),
-  class = c("event_ls", "tbl_df", "tbl", "data.frame"))
+    source.title = x$source.title %||% NA_character_
+  )
+  structure(out, class = c("event_ls", class(out)))
 
 }
 
-#' @export
-print.event_ls <- function(x, ...) {
-  x %>%
-    dplyr::mutate_each(dplyr::funs_(~ truncate_col(.))) %>%
-    print(...)
-}
